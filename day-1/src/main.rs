367b29f0576060b08f06d6e06f8684de46786392
Frank Noirot <frank@kittycad.io>
2023-12-02 11:32:56 -0500

    Day 1 part 1b: don't need capture groups

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,27 +1,57 @@
-use regex::Regex;
-use std::fs;
+use std::num::ParseIntError;
 
-const INPUT: &str = "input.txt";
+use regex::Regex;
 
 fn main() {
-    let contents = fs::read_to_string(INPUT).expect("No file found");
-    let content_newlines = contents.split('\n').filter(|s| !s.is_empty());
+    let input = include_str!("../input.txt");
+    let content_newlines = input.split('\n').filter(|s| !s.is_empty());
 
     let digit_tuples = content_newlines
-        .filter_map(get_outer_digits)
+        .clone()
+        .filter_map(problem_one)
+        .filter_map(parse_str_tuple)
         .filter_map(|(first, last)| format!("{first}{last}").parse::<i32>().ok());
-        // .enumerate().for_each(|(i, n)| println!("{i}: {n}"));
 
-    println!("The sum of all the valid lines' numbers is: {}", digit_tuples.sum::<i32>());
+    // digit_tuples
+    //     .enumerate()
+    //     .for_each(|(i, n)| println!("{i}: {:?}", n));
+
+    println!(
+        "The sum of all the valid lines' numbers is: {}",
+        digit_tuples.sum::<i32>()
+    );
 }
 
-fn get_outer_digits(input: &str) -> Option<(&str, &str)> {
-    // Create a Regex that captures two digits, with zero or more non-digits outside,
-    // and zero or more of any digit between them.
-    let re = Regex::new(r"\D*(\d)(.*(\d)\D*)?").unwrap();
-    let captures = re.captures(input)?;
+fn parse_str_tuple(str_tuple: (&str, &str)) -> Option<(i32, i32)> {
+    match (parse_digit_word(str_tuple.0), parse_digit_word(str_tuple.1)) {
+        (Ok(first), Ok(last)) => Some((first, last)),
+        (_, _) => None,
+    }
+}
 
-    match (captures.get(1), captures.get(3)) {
+fn parse_digit_word(word_or_digit: &str) -> Result<i32, ParseIntError> {
+    match word_or_digit.parse::<i32>() {
+        Ok(num) => Ok(num),
+        Err(e) => match word_or_digit {
+            "one" => Ok(1),
+            "two" => Ok(2),
+            "three" => Ok(3),
+            "four" => Ok(4),
+            "five" => Ok(5),
+            "six" => Ok(6),
+            "seven" => Ok(7),
+            "eight" => Ok(8),
+            "nine" => Ok(9),
+            _ => Err(e),
+        },
+    }
+}
+
+fn problem_one(input: &str) -> Option<(&str, &str)> {
+    let re = Regex::new(r"\d").unwrap();
+    let matches: Vec<regex::Match<'_>> = re.find_iter(input).collect();
+
+    match (matches.first(), matches.last()) {
         (Some(first_match), Some(second_match)) => {
             Some((first_match.as_str(), second_match.as_str()))
         }
