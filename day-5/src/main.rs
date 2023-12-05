67493037d67ae819b7d949cf79dda65b8fd1d7f0
Frank Noirot <frank@kittycad.io>
2023-12-05 06:12:51 -0500

    Day 5 part 1

---
+++ day-5/src/main.rs
@@ -0,0 +1,129 @@
+#[derive(Debug, PartialEq)]
+struct MapLine {
+    dest_start: i64,
+    src_start: i64,
+    length: i64,
+}
+
+fn main() {
+    let input = include_str!("../input.txt");
+    let mut sections = input.split_terminator("\n\n");
+    let seeds = sections
+        .next()
+        .unwrap()
+        .split_whitespace()
+        .filter_map(|n| n.parse::<i64>().ok());
+
+    let section_maps = sections.map(parse_section).map(build_section_fn);
+
+    let destinations = seeds.map(|s| {
+        section_maps.clone().fold(s, |input, func| func(input))
+    });
+
+    println!("The nearest destination is {}", destinations.min().unwrap());
+}
+
+fn build_section_fn(sections: Vec<MapLine>) -> impl Fn(i64) -> i64 {
+    move |input| {
+        for section in &sections {
+            if input >= section.src_start && input < section.src_start + section.length {
+                return section.dest_start + input - section.src_start;
+            }
+        }
+
+        return input;
+    }
+}
+
+fn parse_section(section: &str) -> Vec<MapLine> {
+    let mut lines = section.lines();
+    lines.next(); // We can call next to ignore the first (labeling) line
+
+    lines.map(parse_map_line).collect()
+}
+
+fn parse_map_line(line: &str) -> MapLine {
+    let nums: Vec<i64> = line
+        .split_whitespace()
+        .filter_map(|n| n.parse::<i64>().ok())
+        .collect();
+
+    MapLine {
+        dest_start: nums[0],
+        src_start: nums[1],
+        length: nums[2],
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn line_parsing() {
+        let test = "50 98 2";
+
+        assert_eq!(
+            parse_map_line(test),
+            MapLine {
+                dest_start: 50,
+                src_start: 98,
+                length: 2,
+            }
+        );
+
+        let test_mid_zero = "3378130613 0 34101494";
+        assert_eq!(
+            parse_map_line(test_mid_zero),
+            MapLine {
+                dest_start: 3378130613,
+                src_start: 0,
+                length: 34101494,
+            }
+        )
+    }
+
+    #[test]
+    fn section_parsing() {
+        let test = "seed-to-soil map:\n50 98 2\n52 50 48\n";
+
+        assert_eq!(
+            parse_section(test),
+            vec![
+                MapLine {
+                    dest_start: 50,
+                    src_start: 98,
+                    length: 2,
+                },
+                MapLine {
+                    dest_start: 52,
+                    src_start: 50,
+                    length: 48,
+                },
+            ]
+        );
+    }
+
+    #[test]
+    fn section_fn_building() {
+        let test = vec![
+            MapLine {
+                dest_start: 50,
+                src_start: 98,
+                length: 2,
+            },
+            MapLine {
+                dest_start: 52,
+                src_start: 50,
+                length: 48,
+            },
+        ];
+
+        let test_fn = build_section_fn(test);
+
+        assert_eq!(test_fn(79), 81);
+        assert_eq!(test_fn(14), 14);
+        assert_eq!(test_fn(55), 57);
+        assert_eq!(test_fn(13), 13);
+    }
+}
